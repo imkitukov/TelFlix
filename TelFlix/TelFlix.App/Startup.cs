7eb990cb33ad2760747fce618b62ced2c69e21dc
Dimityr Zh <dimityr32@gmail.com>
2018-11-09 12:31:15 +0200

    add reviews to movies

--- TelFlix/TelFlix.App/Startup.cs
+++ TelFlix/TelFlix.App/Startup.cs
@@ -85,6 +85,8 @@ namespace TelFlix.App
             services.AddTransient<IAddMovieService, AddMovieService>();
             services.AddTransient<IGenreServices, GenreServices>();
             services.AddTransient<IActorServices, ActorServices>();
+            services.AddTransient<IReviewService, ReviewService>();
+
 
             //services.AddScoped<IModifyMovieServices, ModifyMovieServices>();
             //services.AddScoped<ISeedDatabaseService, SeedDatabaseService>();
@@ -162,13 +164,9 @@ namespace TelFlix.App
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                  name: "areas",
-                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-                );
-            });
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-            app.UseMvc(routes =>
-            {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");

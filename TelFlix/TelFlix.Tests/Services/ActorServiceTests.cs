76cfd44824d652a2a459388771f2a310b456d76e
Dimityr Zh <dimityr32@gmail.com>
2018-11-11 21:59:55 +0200

    unit tests start

---
+++ TelFlix/TelFlix.Tests/Services/ActorServiceTests.cs
@@ -0,0 +1,208 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TelFlix.App.Controllers;
+using TelFlix.Data.Context;
+using TelFlix.Data.Models;
+using TelFlix.Services;
+using TelFlix.Services.Contracts;
+using TelFlix.Services.Providers.Exceptions;
+
+namespace TelFlix.Tests.Services
+{
+
+    [TestClass]
+    public class ActorServiceTests
+    {
+        private const string firstActorName = "Pesho Peshov";
+        private const string secondActorName = "Pesho Peshkov";
+        private const string nameWithoutP = "Esho Eshkov";
+        private const int actorId = 1;
+
+        [TestMethod]
+        public void FindActorByIdShould_FindActorCorrect()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            var actor = new Actor()
+            {
+                Id = actorId,
+                FullName = firstActorName
+            };
+            db.Actors.Add(actor);
+            db.SaveChanges();
+
+            var result = actorService.FindActorById(1);
+            Assert.AreEqual(firstActorName, result.FullName);
+        }
+        [TestMethod]
+        public void FindActorByNameShould_FindActorCorrect()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            var actor = new Actor()
+            {
+                Id = actorId,
+                FullName = firstActorName
+            };
+            db.Actors.Add(actor);
+            db.SaveChanges();
+
+            var result = actorService.FindActorByName(firstActorName);
+            Assert.AreEqual(1, result.Id);
+        }
+        [TestMethod]
+        public void AddActorShould_AddActorCorrect()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            var actor = new Actor()
+            {
+                Id = actorId,
+                FullName = firstActorName
+            };
+
+            var result = actorService.AddActor(actor);
+
+            Assert.AreEqual(1, result.Id);
+        }
+        [TestMethod]
+        public void AddActorShould_ThrowWhenActorExist()
+        {
+
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            Actor actorPesho = new Actor
+            {
+                ApiActorId = actorId,
+                FullName = firstActorName
+
+            };
+
+            actorService.AddActor(actorPesho);
+
+            Assert.ThrowsException<EntityAlreadyExistingException>(() => actorService.AddActor(actorPesho));
+        }
+        [TestMethod]
+        public void AddActorShould_RestoreActor()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            Actor actorPesho = new Actor
+            {
+                ApiActorId = actorId,
+                FullName = firstActorName
+            };
+
+            actorService.AddActor(actorPesho);
+
+            actorPesho.IsDeleted = true;
+
+            actorService.AddActor(actorPesho);
+
+            Assert.IsFalse(actorPesho.IsDeleted);
+        }
+        [TestMethod]
+        public void AddActorDetails_ShouldAddDetails()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            var actorPesho = new Actor()
+            {
+                Id = actorId
+            };
+            var actorWithDetails = new Actor()
+            {
+                Id = actorId,
+                DateOfBirth = "Some nice day",
+                Biography = "Some nice words"
+            };
+
+            db.Actors.Add(actorPesho);
+            db.SaveChanges();
+
+
+            Assert.IsNull(actorPesho.Biography);
+            Assert.IsNull(actorPesho.DateOfBirth);
+
+            actorService.AddActorDetails(actorWithDetails);
+
+            Assert.IsNotNull(actorPesho.Biography);
+            Assert.IsNotNull(actorPesho.DateOfBirth);
+        }
+        [TestMethod]
+        public void _ListAllActors_ShouldReturnActorsWithCorrectName()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            var firstActor = new Actor()
+            {
+                Id = 1,
+                FullName = firstActorName
+            };
+            var secondActor = new Actor()
+            {
+                Id = 2,
+                FullName = secondActorName
+            };
+            var thirdActor = new Actor()
+            {
+                Id = 3,
+                FullName = nameWithoutP
+            };
+            db.Actors.AddRange(firstActor, secondActor, thirdActor);
+            db.SaveChanges();
+            var result = actorService.ListAllActors(1, 2, "Pesho").ToList();
+
+            Assert.AreEqual(2, result.Count());
+        }
+        [TestMethod]
+        public void _ListAllActors_ShouldReturnAllActorsWithWhiteSpace()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var actorService = new ActorServices(db);
+
+            var firstActor = new Actor()
+            {
+                Id = 1,
+                FullName = firstActorName
+            };
+            var secondActor = new Actor()
+            {
+                Id = 2,
+                FullName = secondActorName
+            };
+            var thirdActor = new Actor()
+            {
+                Id = 3,
+                FullName = nameWithoutP
+            };
+            db.Actors.AddRange(firstActor, secondActor, thirdActor);
+            db.SaveChanges();
+            var result = actorService.ListAllActors(1, 5, " ").ToList();
+
+            Assert.AreEqual(3, result.Count());
+        }
+        private DbContextOptions<TFContext> DatabaseSimulator()
+        {
+            var dbOptions = new DbContextOptionsBuilder<TFContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return dbOptions;
+        }
+        
+    }
+}
---
+++ TelFlix/TelFlix.Tests/Services/AddMovieServiceTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TelFlix.Data.Context;
+using TelFlix.Data.Models;
+using TelFlix.Services;
+using TelFlix.Services.Contracts;
+using TelFlix.Services.Providers.Exceptions;
+
+namespace TelFlix.Tests.Services
+{
+    [TestClass]
+    public class AddMovieServiceTests
+    {
+        private const string movieTitle = "The best movie ever!";
+        private const int apiMovieIdToUse = 1;
+        [TestMethod]
+        public void AddMovie_ShouldThrow_WhenMovieIsAlreadyThere()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var genreServicesMock = new Mock<IGenreServices>();
+            var actorServicesMock = new Mock<IActorServices>();
+            var movieService = new AddMovieService
+                (db, genreServicesMock.Object, actorServicesMock.Object);
+
+            Movie movieToAdd = new Movie
+            {
+                ApiMovieId = apiMovieIdToUse,
+                Title = movieTitle
+
+            };
+
+            movieService.AddMovie(movieToAdd);
+
+            Assert.ThrowsException<EntityAlreadyExistingException>(() => movieService.AddMovie(movieToAdd));
+
+        }
+        [TestMethod]
+        public void AddMovie_Should_RestoreActor()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var genreServicesMock = new Mock<IGenreServices>();
+            var actorServicesMock = new Mock<IActorServices>();
+            var movieService = new AddMovieService
+                (db, genreServicesMock.Object, actorServicesMock.Object);
+
+            Movie movieToAdd = new Movie
+            {
+                ApiMovieId = apiMovieIdToUse,
+                Title = movieTitle
+            };
+
+            movieService.AddMovie(movieToAdd);
+
+            movieToAdd.IsDeleted = true;
+
+            movieService.AddMovie(movieToAdd);
+
+            Assert.IsFalse(movieToAdd.IsDeleted);
+        }
+        [TestMethod]
+        public void AddMovie_Should_AddMovieWhenItIsCorrect()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var genreServicesMock = new Mock<IGenreServices>();
+            var actorServicesMock = new Mock<IActorServices>();
+            var movieService = new AddMovieService
+                (db, genreServicesMock.Object, actorServicesMock.Object);
+
+            var movieToAdd = new Movie()
+            {
+                ApiMovieId = apiMovieIdToUse,
+                Title = movieTitle
+            };
+
+            var result = movieService.AddMovie(movieToAdd);
+
+            Assert.AreEqual(movieTitle, result.Title);
+            Assert.AreEqual(apiMovieIdToUse, result.ApiMovieId);
+
+        }
+        [TestMethod]
+        public void AddActorToMovie_Should_MakeConnectionsIfActorAlreadyExist()
+        {
+            var db = new TFContext(this.DatabaseSimulator());
+            var genreServicesMock = new Mock<IGenreServices>();
+            var actorServicesMock = new Mock<IActorServices>();
+            var movieService = new AddMovieService
+                (db, genreServicesMock.Object, actorServicesMock.Object);
+            
+           
+            var movie = new Movie()
+            {
+                Id = 1,
+                Title = movieTitle
+            };
+
+            var actorToAdd = new Actor()
+            {
+                Id = 2,
+                ApiActorId = 12,
+                FullName = "Pesho Peshov"
+            };
+            db.Actors.Add(actorToAdd);
+            db.SaveChanges();
+            
+            var actorsCast = new List<(Actor, string)>
+            {
+                (actorToAdd, "a")
+            };
+
+            movieService.AddActorsToMovie(movie, actorsCast);
+
+            Assert.AreEqual(1, db.MoviesActors.Count(a => a.ActorId == 2 && a.MovieId == 1));
+        }
+
+
+        private DbContextOptions<TFContext> DatabaseSimulator()
+        {
+            var dbOptions = new DbContextOptionsBuilder<TFContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return dbOptions;
+        }
+    }
+}
